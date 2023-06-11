9613ef8abbc4d737c2803ec19904e0fc78bce4f6
Rohit Joshi <80029010+rohitjoshi2498@users.noreply.github.com>
2023-06-11 20:13:26 +0530

    changed / to login

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route ,Navigate} from "react-router-dom";
 import Login from "./components/Login";
 import Dashboard from "./components/Dashboard";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route exact path="/" element={<Login></Login>} />
-        <Route exact path="/login/" element={<Login></Login>} />
-        <Route exact path="/dashboard/" element={<Dashboard></Dashboard>} />
+        <Route  path="/" element={<Navigate to ="/login/"/>} />
+        <Route  path="/login/" element={<Login></Login>} />
+        <Route  path="/dashboard/" element={<Dashboard></Dashboard>} />
       </Routes>
     </BrowserRouter>
   );
