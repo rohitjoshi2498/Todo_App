00414aaacd0bd83913465aaa7a5619518bc474a0
Rohit Joshi <80029010+rohitjoshi2498@users.noreply.github.com>
2023-06-11 19:53:33 +0530

    chnaged to / in packagejson

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import Login from './components/Login';
 import Login from "./components/Login";
 import Dashboard from "./components/Dashboard";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-      <Route exact path="/" element={<Login></Login>} />
+        <Route exact path="/" element={<Login></Login>} />
         <Route exact path="/login/" element={<Login></Login>} />
         <Route exact path="/dashboard/" element={<Dashboard></Dashboard>} />
       </Routes>
